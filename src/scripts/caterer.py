baddcc36303bb0458ac807086844523835fa73f9
Hrishikesh S <hrishikesh123s@gmail.com>
2019-10-24 02:20:21 +0530

    adding code to view customers & caterers

--- src/scripts/caterer.py
+++ src/scripts/caterer.py
@@ -1,1 +1,9 @@
 # all endpoints related to caterer's view
+
+# list all available caterers
+@app.route('/showAllCaterers', methods = ['GET'])
+def showAllCaterers():
+    if request.method == 'GET':
+        caterers = db['caterers']
+        data = caterers.find({})
+        return jsonify(data)
--- src/scripts/customer.py
+++ src/scripts/customer.py
@@ -1,1 +1,12 @@
 # all endpoints related to customer view
+# assuming the connection to database is called client
+# where client = MongoClient('localhost', ...)
+# and db = client.lunchbox where lunchbox is the name of the database
+
+# list all available canteens
+@app.route('/showAllCanteens', methods = ['GET'])
+def showAllCanteens():
+    if request.method == 'GET':
+        canteens = db['canteens']
+        data = canteens.find({})
+        return jsonify(data)
