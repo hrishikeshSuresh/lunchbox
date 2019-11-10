907b12359ba24b927dc9c9f2b0961ae8fe11e35e
Harshith Arunkumar <harshith712@gmail.com>
2019-11-10 19:39:06 +0530

    Receiving and accepting orders

--- src/scripts/login.py
+++ src/scripts/login.py
@@ -5,7 +5,6 @@ from db_connector import *
 from datetime import datetime
 import string, random
 
-app = Flask(__name__) #create the Flask app
 
 @app.route('/api/v1/login', methods = [ 'POST' ]   )
 def login():
@@ -19,15 +18,14 @@ def login():
             N = 7 
             res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N)) 
             session [ 'id' ] = res
-            session['logged_in'] = True'''
+            session['logged_in'] = True''' #This code is just in case we need to change it to sessions
 
             resp = make_response(None, 200)  
             resp.set_cookie('username',username)  
             return resp  
 
-            #return jsonify({ 'User logged in': True }), 200; 
 
         else:
             return jsonify ( { 'Invalid credentials' }), 401
 
-        #return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)
+       
