4f40693d5ef596dcd15da26238668324c95927ed
pixlx <kunalplw@gmail.com>
2019-11-18 21:10:01 +0530

    adding API functionality to view all orders

--- src/webui/src/caterer/screens/Orders.js
+++ src/webui/src/caterer/screens/Orders.js
@@ -1,17 +1,23 @@
 import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Image,
-  TouchableWithoutFeedback,
-  ImageBackground,
-  Dimensions
-} from "react-native";
+import {ScrollView, StyleSheet, Image, TouchableWithoutFeedback, ImageBackground, Dimensions, Text, View } from "react-native";
 //galio
 import { Block, Text, theme } from "galio-framework";
 //argon
 import { articles, Images, argonTheme } from "../../constants/";
 import { Card } from "../../components/";
+import { Accordion, Block } from 'galio-framework';
 
 
+class Orders extends React.Component {
+    state = {
+        myState: 'This view is for all the orders' }
+   render() {
+      return (
+         <View>
+            <Text> {this.state.myState}</Text>
+         </View>
+
+      );
+   }
+}
 export default Orders;
